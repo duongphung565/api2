35f741fed68f56fd2b97bcfbc1ba39ef898e4e19
duongphung565 <nguyenphungduong565@gmail.com>
2019-01-26 10:03:36 +0700

    Relationship

--- app/Model/Product.php
+++ app/Model/Product.php
@@ -3,8 +3,10 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
-
+use App\Model\Reviews;
 class Product extends Model
 {
-    //
+    public function reviews(){
+      return $this->hasMany(Reviews::class);
+    }
 }
--- database/migrations/2019_01_24_095038_create_products_table.php
+++ database/migrations/2019_01_24_095038_create_products_table.php
@@ -15,7 +15,7 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('name')->nullable();
             $table->text('detail');
             $table->integer('price');
             $table->integer('stock');
